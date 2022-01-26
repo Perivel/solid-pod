44f46f30261571dee20b5c6d61e6cca0a34c9542
PatrickLumenus <patrickaluy@gmail.com>
2022-01-26 16:52:13 -0500

    Bug fixes with the build

--- packages/solidus/rollup.config.js
+++ packages/solidus/rollup.config.js
@@ -1,32 +1,69 @@
 import babel from "@rollup/plugin-babel";
 import nodeResolve from "@rollup/plugin-node-resolve";
+import solidRollupConfig from 'rollup-preset-solid';
+import { terser } from 'rollup-plugin-terser';
+import { resolve } from 'path';
 
-export default {
-  input: "index.ts",
+// const old = {
+//   input: "index.ts",
+//   output: [
+//     {
+//       dir: "dist",
+//       format: "umd",
+//       name: "solidus"
+//     }
+//   ],
+//   external: [
+//       "solid-js", 
+//       "solid-js/web",
+//       "@swindle/core",
+//       "@swindle/color",
+//       "polka",
+//       "serve-static",
+//       "solid-app-router",
+//       "solid-meta"
+//     ],
+//   plugins: [
+//     nodeResolve({
+//       extensions: [".js", ".ts", ".tsx"],
+//       exportConditions: ['solid']
+//     }),
+//     babel({
+//       extensions: [".js", ".ts", ".tsx"],
+//       babelHelpers: "bundled",
+//       presets: ["solid", "@babel/preset-typescript"],
+//       exclude: "node_modules/**"
+//     })
+//   ]
+// };
+
+const custom = {
+  input: 'index.ts',
+  external: [
+    "@swindle/core",
+    "@swindle/color",
+    "polka",
+    "serve-static",
+  ],
   output: [
     {
-      file: "dist/index.js",
+      format: "cjs",
+      dir: resolve("dist/cjs"),
+      sourcemap: true,
+    },
+    {
+      format: "esm",
+      dir: resolve("dist/esm"),
+      sourcemap: true,
+    },
+    {
+      name: "solidus",
       format: "umd",
-      name: "solidus"
-    }
-  ],
-  external: [
-      "solid-js", 
-      "solid-js/web",
-      "@swindle/core",
-      "@swindle/color",
-      "polka",
-      "serve-static"
-    ],
-  plugins: [
-    nodeResolve({
-      extensions: [".js", ".ts", ".tsx"]
-    }),
-    babel({
-      extensions: [".js", ".ts", ".tsx"],
-      babelHelpers: "bundled",
-      presets: ["solid", "@babel/preset-typescript"],
-      exclude: "node_modules/**"
-    })
+      dir: resolve("dist/umd"),
+      sourcemap: true,
+      plugins: [terser()],
+    },
   ]
-};
+}
+
+export default solidRollupConfig(custom);
